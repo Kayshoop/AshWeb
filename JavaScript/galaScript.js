76a5bfb04a115f955c313cbc819abeece0f19cee
Glob~Lynn(Kayshoop) <shoopkayla4@gmail.com>
2024-12-22 12:15:47 -0600

    adding function for service selection

--- JavaScript/galaScript.js
+++ JavaScript/galaScript.js
@@ -1,6 +1,5 @@
-// building gala for services
 document.addEventListener('DOMContentLoaded', function() {
-    const service = [
+    const services = [
         {title: 'EYEBROW', description: 'Waxing the brow.', price: '$15', time: '20 min'},
         {title: 'CHIN', description: 'beard area', price: '$15', time: '20 MIN'},
         {title: 'CHEEKS', description: 'face cheeks', price: '$15', time: '20 MIN'},
@@ -11,26 +10,41 @@ document.addEventListener('DOMContentLoaded', function() {
         {title: 'FULL BUTT', description: 'butt wax', price: '$20', time: '10 MIN'},
         {title: 'FULL ARM', description: 'shoulder to wrist', price: '$70', time: '40 MIN'},
         {title: 'HALF ARM', description: 'bellow the elbow', price: '$40', time: '25 MIN'},
-
         {title: 'FULL LEG', description: 'hip to ankle wax', price: '$95', time: '60 MIN'},
         {title: 'UPPER LEG', description: 'above the knee wax', price: '$55', time: '35 MIN'},
         {title: 'LOWER LEG', description: 'bellow the knee wax', price: '$60', time: '40 MIN'},
-
     ]; // there are 73 services
 
     const gallery = document.getElementsByClassName('serviceGala')[0];
+    const serviceList = document.getElementsByClassName('serviceList')[0];
 
-    service.forEach(service => {
+    services.forEach(service => {
         const serviceBox = document.createElement('div');
         serviceBox.classList.add('service-box');
 
         serviceBox.innerHTML = `
-        <h3>${service.title}</h3>
-        <p>${service.description}</p>
-        <p><strong>Price:</strong>${service.price}</p>
-        <p><strong>Time:</strong>${service.time}</p>
+            <h3>${service.title}</h3>
+            <p>${service.description}</p>
+            <p><strong>Price:</strong> ${service.price}</p>
+            <p><strong>Time:</strong> ${service.time}</p>
         `;
 
-    gallery.appendChild(serviceBox);
+        serviceBox.addEventListener('click', function() {
+            displayService(service.title, service.price);
+        });
+
+        gallery.appendChild(serviceBox);
     });
+
+    function displayService(title, price) {
+        const selectedService = document.createElement('div');
+        selectedService.classList.add('selected-service');
+
+        selectedService.innerHTML = `
+            <br>
+            <h3>${title}:</h3>
+            <p>${price}</p>
+        `;
+        serviceList.appendChild(selectedService);
+    }
 });
